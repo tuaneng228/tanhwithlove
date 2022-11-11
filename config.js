4715f302616b1166367ca3e07314e42dbbd3ab6c
tuaneng228 <114272936+tuaneng228@users.noreply.github.com>
2022-11-11 16:52:45 +0700

    ok

---
+++ config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+    introTitle: 'HellO Thanh Hằng!',
+    introDesc: `Welcome to my heart <3`,
+    btnIntro: 'From TuanEng with love',
+    reply: 'Quá tốt bụng, quá sức tinh tế hihi <33333333',
+    mess: 'WOAAAA 🥰. Anh biết mà 😘.',
+    messDesc: 'Chúc em buổi tối vui vẻ nha <3',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://m.me/nta228' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
